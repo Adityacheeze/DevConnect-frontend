3579823eb4d999dba8d108b395a8d31f407c8fa8
Adityacheeze <adityamgupta4@gmail.com>
2025-07-24 16:02:20 +0530

    Login PAGE with API integration and Fixing CORS, setting up cookies

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # devTinder Frontend
+
 - Create Vite + React Project
 - Remove unncessary code
 - Initialize git repo
@@ -9,3 +10,62 @@
 - Setup Routing
 - Use nested routing and render different components using <Outlet/>
 - Create Footer.jsx
+- Install Axios for making API Calls
+
+### Routing 
+- In App.jsx : 
+```
+function App() {
+  return (
+    <>
+      <BrowserRouter basename="/">
+        <Routes>
+          <Route path="/" element={<Body/>}>
+            <Route path="/login" element={<Login/>}></Route>
+            <Route path="/profile" element={<Profile/>}></Route>
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+```
+- In Body.jsx : 
+```
+const Body = () => {
+  return (
+    <>
+     <NavBar/>
+     <Outlet/> 
+     <Footer/>
+    </>
+  )
+}
+```
+
+### Backend : Fixing CORS
+- Install Cors in backend
+- In Backend => use cors middleware with `origin set up` and `credentials = true`
+
+```
+app.use(
+  cors(
+    {
+      origin: "http://localhost:5173",
+      credentials: true,
+    }
+  )
+);
+```
+### Frontend : Fixing CORS
+- In Frontend while making the API call pass `withCredentials = true`
+```
+const res = axios.post(
+  "http://localhost:3000/login",
+  {
+    email,
+    password,
+  },
+  { withCredentials: true }
+);
+```
--- src/Login.jsx
+++ src/Login.jsx
@@ -1,11 +1,69 @@
-import React from 'react'
+import axios from "axios";
+import React, { useState } from "react";
 
 const login = () => {
+  const [email, setEmail] = useState("anshika@gmail.com");
+  const [password, setPassword] = useState("Anshika@123");
+  const handleLogin = async () => {
+    try {
+      const res = axios.post(
+        "http://localhost:3000/login",
+        {
+          email,
+          password,
+        },
+        { withCredentials: true }
+      );
+    } catch (error) {
+      console.error(error);
+    }
+  };
   return (
-    <div>
-      <h1>LOGIN PAGE</h1>
+    <div className="flex justify-center">
+      <div className="card bg-base-300 w-96 shadow-sm my-10">
+        {/* <figure>
+          <img
+            src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+            alt="Shoes"
+          />
+        </figure> */}
+        <div className="card-body">
+          <h2 className="card-title justify-center">Login</h2>
+          <div className="py-4">
+            <fieldset className="fieldset">
+              <legend className="fieldset-legend">Email ID</legend>
+              <input
+                type="text"
+                className="input"
+                value={email}
+                placeholder="Type here"
+                onChange={(e) => {
+                  setEmail(e.target.value);
+                }}
+              />
+            </fieldset>
+            <fieldset className="fieldset">
+              <legend className="fieldset-legend">Password</legend>
+              <input
+                type="text"
+                className="input"
+                value={password}
+                placeholder="Type here"
+                onChange={(e) => {
+                  setPassword(e.target.value);
+                }}
+              />
+            </fieldset>
+          </div>
+          <div className="card-actions justify-center">
+            <button className="btn btn-primary" onClick={handleLogin}>
+              Login
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default login
+export default login;
