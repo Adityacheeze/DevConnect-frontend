6d6d344c299f600ae692c9ad037ae36b5d018259
Adityacheeze <adityamgupta4@gmail.com>
2025-07-25 18:55:32 +0530

    bug fix in footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Footer() {
   return (
-    <footer className="footer sm:footer-horizontal bg-base-300 text-neutral-content p-4 bottom-0">
+    <footer className="footer sm:footer-horizontal bg-base-300 text-neutral-content p-4 fixed bottom-0">
       <aside className="grid-flow-col items-center">
         <svg
           width="36"
